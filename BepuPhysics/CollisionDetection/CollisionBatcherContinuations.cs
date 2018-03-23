a287bd45f9de72ceda358ef99d4ae05ec488f596
Ross Nordby <norbo@bepuentertainment.com>
2018-03-22 19:34:22 -0500

    Refactored collision batcher a little bit to be less of a monolith.

---
+++ BepuPhysics/CollisionDetection/CollisionBatcherContinuations.cs
@@ -0,0 +1,120 @@
+﻿using BepuUtilities.Memory;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BepuPhysics.CollisionDetection
+{
+    public interface ICollisionTestContinuation
+    {
+        void Create(int slots, BufferPool pool);
+
+        unsafe void OnChildCompleted<TCallbacks>(ref PairContinuation report, ConvexContactManifold* manifold, ref CollisionBatcher<TCallbacks> batcher)
+            where TCallbacks : struct, ICollisionCallbacks;
+        unsafe void OnChildCompletedEmpty<TCallbacks>(ref PairContinuation report, ref CollisionBatcher<TCallbacks> batcher)
+            where TCallbacks : struct, ICollisionCallbacks;
+    }
+
+    /// <summary>
+    /// Describes the flow control to apply to a convex-convex pair report.
+    /// </summary>
+    public enum CollisionContinuationType : byte
+    {
+        /// <summary>
+        /// Marks a pair as requiring no further processing before being reported to the user supplied continuations.
+        /// </summary>
+        Direct,
+        /// <summary>
+        /// Marks a pair as part of a set of a higher (potentially multi-manifold) pair, potentially requiring contact reduction.
+        /// </summary>
+        NonconvexReduction,
+        //TODO: We don't yet support boundary smoothing for meshes or convexes. Most likely, boundary smoothed convexes won't make it into the first release of the engine at all;
+        //they're a pretty experimental feature with limited applications.
+        ///// <summary>
+        ///// Marks a pair as a part of a set of mesh-convex collisions, potentially requiring mesh boundary smoothing.
+        ///// </summary>
+        //BoundarySmoothedMesh,
+        ///// <summary>
+        ///// Marks a pair as a part of a set of convex-convex collisions, potentially requiring general convex boundary smoothing.
+        ///// </summary>
+        //BoundarySmoothedConvexes,           
+
+    }
+
+    public struct PairContinuation
+    {
+        public int PairId;
+        public int ChildA;
+        public int ChildB;
+        public uint Packed;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public PairContinuation(int pairId, int childA, int childB, CollisionContinuationType continuationType, int continuationIndex)
+        {
+            PairId = pairId;
+            ChildA = childA;
+            ChildB = childB;
+            Debug.Assert(continuationIndex < (1 << 23));
+            Packed = (uint)(((int)continuationType << 24) | continuationIndex);
+        }
+        public PairContinuation(int pairId)
+        {
+            PairId = pairId;
+            ChildA = 0;
+            ChildB = 0;
+            Packed = 0;
+        }
+
+        public CollisionContinuationType Type { [MethodImpl(MethodImplOptions.AggressiveInlining)] get { return (CollisionContinuationType)(Packed >> 24); } }
+        public int Index { [MethodImpl(MethodImplOptions.AggressiveInlining)] get { return (int)(Packed & 0x007FFFFF); } }
+    }
+
+    public struct BatcherContinuations<T> where T : ICollisionTestContinuation
+    {
+        public Buffer<T> Continuations;
+        public IdPool<Buffer<int>> IdPool;
+        const int InitialCapacity = 64;
+
+        public ref T CreateContinuation(int slotsInContinuation, BufferPool pool, out int index)
+        {
+            if (!Continuations.Allocated)
+            {
+                Debug.Assert(!IdPool.AvailableIds.Span.Allocated);
+                //Lazy initialization.
+                pool.Take(InitialCapacity, out Continuations);
+                IdPool<Buffer<int>>.Create(pool.SpecializeFor<int>(), InitialCapacity, out IdPool);
+            }
+            index = IdPool.Take();
+            if (index >= Continuations.Length)
+            {
+                pool.Resize(ref Continuations, index, index - 1);
+            }
+            ref var continuation = ref Continuations[index];
+            continuation.Create(slotsInContinuation, pool);
+            return ref Continuations[index];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe void ContributeChildToContinuation<TCallbacks>(ref PairContinuation continuation, ConvexContactManifold* manifold, ref CollisionBatcher<TCallbacks> batcher)
+            where TCallbacks : struct, ICollisionCallbacks
+        {
+            Continuations[continuation.Index].OnChildCompleted(ref continuation, manifold, ref batcher);
+        }
+
+
+        internal void Dispose(BufferPool pool)
+        {
+            if (Continuations.Allocated)
+            {
+                pool.ReturnUnsafely(Continuations.Id);
+                Debug.Assert(IdPool.AvailableIds.Span.Allocated);
+                IdPool.Dispose(pool.SpecializeFor<int>());
+            }
+#if DEBUG
+            //Makes it a little easier to catch bad accesses.
+            this = new BatcherContinuations<T>();
+#endif
+        }
+    }
+}
---
+++ BepuPhysics/CollisionDetection/NonconvexReduction.cs
@@ -0,0 +1,122 @@
+﻿using BepuUtilities.Memory;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BepuPhysics.CollisionDetection
+{
+    public struct NonconvexReductionChild
+    {
+        public ConvexContactManifold Manifold;
+        /// <summary>
+        /// Offset from the origin of the first shape's parent to the child's location in world space. If there is no parent, this is the zero vector.
+        /// </summary>
+        public Vector3 OffsetA;
+        /// <summary>
+        /// Offset from the origin of the second shape's parent to the child's location in world space. If there is no parent, this is the zero vector.
+        /// </summary>
+        public Vector3 OffsetB;
+    }
+
+    public struct NonconvexReduction : ICollisionTestContinuation
+    {
+        public int ChildCount;
+        public int CompletedChildCount;
+        public Buffer<NonconvexReductionChild> Children;
+
+        public void Create(int childManifoldCount, BufferPool pool)
+        {
+            ChildCount = childManifoldCount;
+            CompletedChildCount = 0;
+            pool.Take(childManifoldCount, out Children);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe void FlushIfCompleted<TCallbacks>(int pairId, ref CollisionBatcher<TCallbacks> batcher) where TCallbacks : struct, ICollisionCallbacks
+        {
+            ++CompletedChildCount;
+            Debug.Assert(ChildCount > 0);
+            if (ChildCount == CompletedChildCount)
+            {
+                //This continuation is ready for processing. Find which contact manifold to report.
+                int populatedChildManifolds = 0;
+                //We cache an index in case there is only one populated manifold. Order of discovery doesn't matter- this value only gets used when there's one manifold.
+                int samplePopulatedChildIndex = 0;
+                for (int i = 0; i < ChildCount; ++i)
+                {
+                    if (Children[i].Manifold.Count > 0)
+                    {
+                        ++populatedChildManifolds;
+                        samplePopulatedChildIndex = i;
+                    }
+                }
+                var sampleChild = (NonconvexReductionChild*)Children.Memory + samplePopulatedChildIndex;
+                if (populatedChildManifolds > 1)
+                {
+                    //There are multiple contributing child manifolds, so just assume that the resulting manifold is going to be nonconvex.
+                    NonconvexContactManifold reducedManifold;
+                    //We should assume that the stack memory backing the reduced manifold is uninitialized. We rely on the count, so initialize it manually.
+                    reducedManifold.Count = 0;
+                    for (int i = 0; i < ChildCount; ++i)
+                    {
+                        ref var child = ref Children[i];
+                        ref var contactBase = ref child.Manifold.Contact0;
+                        for (int j = 0; j < child.Manifold.Count; ++j)
+                        {
+                            ref var contact = ref Unsafe.Add(ref contactBase, j);
+                            contact.Offset += child.OffsetA;
+                            //Mix the convex-generated feature id with the child index.
+                            contact.FeatureId ^= i << 8;
+                            NonconvexContactManifold.Add(&reducedManifold, ref child.Manifold.Normal, ref contact);
+                            if (reducedManifold.Count == 8)
+                                break;
+                        }
+                        if (reducedManifold.Count == 8)
+                            break;
+                    }
+                    //The manifold offsetB is the offset from shapeA origin to shapeB origin.
+                    var reducedManifoldPointer = &reducedManifold;
+                    reducedManifold.OffsetB = sampleChild->Manifold.OffsetB - sampleChild->OffsetB + sampleChild->OffsetA;
+                    batcher.Callbacks.OnPairCompleted(pairId, reducedManifoldPointer);
+                }
+                else
+                {
+                    //Two possibilities here: 
+                    //1) populatedChildManifolds == 1, and samplePopulatedChildIndex is the index of that sole populated manifold. We can directly report it.
+                    //It's useful to directly report the convex child manifold for performance reasons- convex constraints do not require multiple normals and use a faster friction model.
+                    //2) populatedChildManifolds == 0, and samplePopulatedChildIndex is 0. Given that we know this continuation is only used when there is at least one manifold expected
+                    //and that we can only hit this codepath if all manifolds are empty, reporting manifold 0 is perfectly fine.
+                    //The manifold offsetB is the offset from shapeA origin to shapeB origin.
+                    sampleChild->Manifold.OffsetB = sampleChild->Manifold.OffsetB - sampleChild->OffsetB + sampleChild->OffsetA;
+                    var contacts = &sampleChild->Manifold.Contact0;
+                    for (int i = 0; i < sampleChild->Manifold.Count; ++i)
+                    {
+                        contacts[i].Offset += sampleChild->OffsetA;
+                    }
+                    batcher.Callbacks.OnPairCompleted(pairId, &sampleChild->Manifold);
+                }
+                batcher.Pool.ReturnUnsafely(Children.Id);
+#if DEBUG
+                //This makes it a little easier to detect invalid accesses that occur after disposal.
+                this = new NonconvexReduction();
+#endif
+            }
+        }
+        public unsafe void OnChildCompleted<TCallbacks>(ref PairContinuation report, ConvexContactManifold* manifold, ref CollisionBatcher<TCallbacks> batcher)
+            where TCallbacks : struct, ICollisionCallbacks
+        {
+            Children[CompletedChildCount].Manifold = *manifold;
+            FlushIfCompleted(report.PairId, ref batcher);
+
+        }
+
+        public void OnChildCompletedEmpty<TCallbacks>(ref PairContinuation report, ref CollisionBatcher<TCallbacks> batcher) where TCallbacks : struct, ICollisionCallbacks
+        {
+            Children[CompletedChildCount] = default;
+            FlushIfCompleted(report.PairId, ref batcher);
+        }
+    }
+}
