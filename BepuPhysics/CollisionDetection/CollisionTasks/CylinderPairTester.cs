4cf49c237c8b93141f54240a835b890584a87b2e
Ross Nordby <norbo@bepuentertainment.com>
2018-12-22 22:56:14 -0600

    Fixed some CylinderPairDepthTester bugs.

--- BepuPhysics/CollisionDetection/CollisionTasks/CylinderPairTester.cs
+++ BepuPhysics/CollisionDetection/CollisionTasks/CylinderPairTester.cs
@@ -14,16 +14,15 @@ namespace BepuPhysics.CollisionDetection.CollisionTasks
         {
             //dot(extremeA - extremeB, N) for a potentially non-unit N.
             //extremeA = (N.X, 0, N.Z) * radiusA / sqrt(N.X^2 + N.Z^2) + (0, N.Y > 0 ? a.HalfLength : -a.HalfLength, 0)
-            //extremeB = localOffsetB + (dot(-N, B.X) * B.X + dot(-N, B.Z) * B.Z) * radiusA / sqrt(dot(-N, B.X)^2 + dot(-N, B.Z)^2) + B.Y * (dot(-N, B.Y) > 0 ? b.HalfLength : -b.HalfLength)
+            //extremeB = localOffsetB + (dot(-N, B.X) * B.X + dot(-N, B.Z) * B.Z) * radiusB / sqrt(dot(-N, B.X)^2 + dot(-N, B.Z)^2) + B.Y * (dot(-N, B.Y) > 0 ? b.HalfLength : -b.HalfLength)
             //dot(N, extremeA) = horizontalNormalLengthA * radiusA + N.Y * (N.Y > 0 ? a.HalfLength : -a.HalfLength)
-            //dot(N, extremeB) = dot(localOffsetB, N) + horizontalNormalLengthB * radiusB + dot(-N, B.Y) * (dot(-N, B.Y) > 0 ? b.HalfLength : -b.HalfLength)
+            //dot(N, extremeB) = dot(localOffsetB, N) - horizontalNormalLengthB * radiusB + dot(N, B.Y) * (dot(-N, B.Y) > 0 ? b.HalfLength : -b.HalfLength)
             var horizontalNormalLengthA = Vector.SquareRoot(normal.X * normal.X + normal.Z * normal.Z);
-            Matrix3x3Wide.TransformByTransposedWithoutOverlap(normal, localOrientationB, out var localNormalB);
-            Vector3Wide.Negate(localNormalB, out localNormalB);
-            var horizontalNormalLengthB = Vector.SquareRoot(localNormalB.X * localNormalB.X + localNormalB.Z * localNormalB.Z);
             var contributionA = horizontalNormalLengthA * a.Radius + normal.Y * Vector.ConditionalSelect(Vector.LessThan(normal.Y, Vector<float>.Zero), -a.HalfLength, a.HalfLength);
+            Matrix3x3Wide.TransformByTransposedWithoutOverlap(normal, localOrientationB, out var localNormalB);
+            var horizontalNormalLengthB = Vector.SquareRoot(localNormalB.X * localNormalB.X + localNormalB.Z * localNormalB.Z);
             Vector3Wide.Dot(localOffsetB, normal, out var offsetDotN);
-            var contributionB = horizontalNormalLengthB * b.Radius + localNormalB.Y * Vector.ConditionalSelect(Vector.LessThan(localNormalB.Y, Vector<float>.Zero), -b.HalfLength, b.HalfLength) + offsetDotN;
+            var contributionB = offsetDotN - horizontalNormalLengthB * b.Radius + localNormalB.Y * Vector.ConditionalSelect(Vector.GreaterThan(localNormalB.Y, Vector<float>.Zero), -b.HalfLength, b.HalfLength);
             scaledDepth = contributionA - contributionB;
         }
     }
