ffe96289519d30ce944a81cbd9b071fef25e1d1d
Ross Nordby <norbo@bepuentertainment.com>
2019-04-20 14:04:42 -0500

    Added simpler/faster/seemingly better nonconvex reduction. MeshReduction seems to have a bug.

--- BepuPhysics/CollisionDetection/NonconvexReduction.cs
+++ BepuPhysics/CollisionDetection/NonconvexReduction.cs
@@ -332,6 +332,168 @@ namespace BepuPhysics.CollisionDetection
             remainingContacts.Dispose(pool);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static float ComputeDistinctiveness(in ConvexContact candidate, in Vector3 contactNormal, in NonconvexContact reducedContact, float distanceSquaredInterpolationMin, float inverseDistanceSquaredInterpolationSpan, float depthScale)
+        {
+            //The more distant a contact is from another contact, or the more different its normal is, the more distinct it is considered.
+            //The goal is for distinctiveness to range from around 0 to 2. The exact values aren't extremely important- we just want a rough range
+            //so that we can meaningfully blend in a depth heuristic.
+            var normalDot = Vector3.Dot(contactNormal, reducedContact.Normal);
+            const float normalInterpolationSpan = -0.99999f;
+            //Normal dots above a threshold are considered completely redundant. acos(0.99999) is about 0.25 degrees.
+            //Normals pointing at a 90 degree angle are given a value of ~1, while being completely opposed gives a value of ~2.
+            var normalDistinctiveness = (normalDot - 0.99999f) * (1f / normalInterpolationSpan);
+
+            //Below a threshold, the offset is considered completely redundant.
+            var offsetDistinctiveness = ((reducedContact.Offset - candidate.Offset).LengthSquared() - distanceSquaredInterpolationMin) * inverseDistanceSquaredInterpolationSpan;
+
+            //We use a three way max across the normal, offset, and normal * offset distinctiveness. Both normal and offset scores can go above 1 sometimes, so it's possible for normal * offset to be higher than the other two.
+            //Note that a point in the exact same position but a significantly different normal is still considered distinct.
+            var combinedDistinctiveness = normalDistinctiveness * offsetDistinctiveness;
+            var distinctiveness = offsetDistinctiveness > normalDistinctiveness ? offsetDistinctiveness : normalDistinctiveness;
+            if (distinctiveness < combinedDistinctiveness)
+                distinctiveness = combinedDistinctiveness;
+
+            if (distinctiveness <= 0)
+            {
+                distinctiveness = 0;
+            }
+            else
+            {
+                //Take into account the depth of the contact. Deeper contacts are more important to the manifold.
+                var depthMultiplier = 1 + candidate.Depth * depthScale;
+                if (depthMultiplier < 0.01f)
+                    depthMultiplier = 0.01f;
+                distinctiveness *= depthMultiplier;
+            }
+            return distinctiveness;
+        }
+
+        unsafe void ChooseMostDistinct(NonconvexContactManifold* manifold, BufferPool pool)
+        {
+            //The end goal of contact reduction is to choose a reasonably stable subset of contacts which offer the greatest degree of constraint.
+            //Computing a globally optimal solution to this would be pretty expensive for any given scoring mechanism, so we'll make some simplifications:
+            //1) Heuristically select a starting point that will be reasonably stable between frames. Add it to the reduced manifold.
+            //2) Search for the contact in the unadded set which maximizes the constraint heuristic and add it. Remove it from the unadded set.
+            //3) Repeat #2 until there is no room left in the reduced manifold or all remaining candidates are redundant with the reduced manifold.
+
+            //We first compute some calibration data. Knowing the approximate center of the manifold and the maximum distance allows more informed thresholds.
+
+            var extentAxis = new Vector3(0.280454652f, 0.55873544499f, 0.7804869574f);
+            float minimumExtent = float.MaxValue;
+            Vector3 minimumExtentPosition = default;
+            for (int i = 0; i < ChildCount; ++i)
+            {
+                ref var child = ref Children[i];
+                for (int j = 0; j < child.Manifold.Count; ++j)
+                {
+                    ref var position = ref Unsafe.Add(ref child.Manifold.Contact0, j).Offset;
+                    var extent = Vector3.Dot(position, extentAxis);
+                    if (extent < minimumExtent)
+                    {
+                        minimumExtent = extent;
+                        minimumExtentPosition = position;
+                    }
+                }
+            }
+            float maximumDistanceSquared = 0;
+            for (int i = 0; i < ChildCount; ++i)
+            {
+                ref var child = ref Children[i];
+                for (int j = 0; j < child.Manifold.Count; ++j)
+                {
+                    var distanceSquared = (Unsafe.Add(ref child.Manifold.Contact0, j).Offset - minimumExtentPosition).LengthSquared();
+                    if (distanceSquared > maximumDistanceSquared)
+                        maximumDistanceSquared = distanceSquared;
+                }
+            }
+            var maximumDistance = (float)Math.Sqrt(maximumDistanceSquared);
+            float initialBestScore = -float.MaxValue;
+            int initialBestScoreIndex = 0;
+            var remainingContacts = new QuickList<RemainingCandidate>(ChildCount * 4, pool);
+            var extremityScale = maximumDistance * 5e-3f;
+            for (int childIndex = 0; childIndex < ChildCount; ++childIndex)
+            {
+                ref var child = ref Children[childIndex];
+                ref var childContactsBase = ref child.Manifold.Contact0;
+                for (int contactIndex = 0; contactIndex < child.Manifold.Count; ++contactIndex)
+                {
+                    ref var contact = ref Unsafe.Add(ref childContactsBase, contactIndex);
+                    //Note that we only consider 'extreme' contacts that have positive depth to avoid selecting purely speculative contacts as a starting point.
+                    //If there are no contacts with positive depth, it's fine to just rely on the 'deepest' speculative contact. 
+                    //Feature id stability doesn't matter much if there is no stable contact.
+                    float candidateScore;
+                    if (contact.Depth >= 0)
+                    {
+                        //Note that we assume that the contact offsets have already been moved into the parent's space in compound pairs so that we can validly compare extents across manifolds.
+                        var extent = Vector3.Dot(contact.Offset, extentAxis) - minimumExtent;
+                        candidateScore = contact.Depth + extent * extremityScale;
+                    }
+                    else
+                    {
+                        //Speculative contact scores are simply based on depth.
+                        candidateScore = contact.Depth;
+                    }
+                    if (candidateScore > initialBestScore)
+                    {
+                        initialBestScore = candidateScore;
+                        initialBestScoreIndex = remainingContacts.Count;
+                    }
+                    ref var indices = ref remainingContacts.AllocateUnsafely();
+                    indices.ChildIndex = childIndex;
+                    indices.ContactIndex = contactIndex;
+                    indices.Uniqueness = float.MaxValue;
+                }
+            }
+
+            Debug.Assert(remainingContacts.Count > 0, "This function should only be called when there are populated manifolds.");
+
+            UseContact(ref remainingContacts, initialBestScoreIndex, ref Children, manifold);
+
+            var distanceSquaredInterpolationMin = maximumDistanceSquared * (1e-3f * 1e-3f);
+            var inverseDistanceSquaredInterpolationSpan = 1f / (maximumDistanceSquared);
+            var depthScale = 200f / maximumDistance;
+
+            //We now have a decent starting point. Now, incrementally search for contacts which expand the manifold as much as possible.
+            //This is going to be a greedy and nonoptimal search, but being consistent and good enough is more important than true optimality.
+            var reducedContacts = &manifold->Contact0;
+            while (remainingContacts.Count > 0 && manifold->Count < NonconvexContactManifold.MaximumContactCount)
+            {
+                var lastContactIndex = manifold->Count - 1;
+                var reducedContact = reducedContacts + lastContactIndex;
+
+                float bestScore = -1;
+                int bestScoreIndex = 0;
+                for (int i = remainingContacts.Count - 1; i >= 0; --i)
+                {
+                    ref var remainingContact = ref remainingContacts[i];
+                    ref var childManifold = ref Children[remainingContact.ChildIndex].Manifold;
+                    ref var childContact = ref Unsafe.Add(ref childManifold.Contact0, remainingContact.ContactIndex);
+                    var distinctiveness = ComputeDistinctiveness(childContact, childManifold.Normal, *reducedContact, distanceSquaredInterpolationMin, inverseDistanceSquaredInterpolationSpan, depthScale);
+                    if (distinctiveness <= 0)
+                    {
+                        //This contact is fully redundant.
+                        remainingContacts.FastRemoveAt(i);
+                    }
+                    else
+                    {
+                        //The contact wasn't fully redundant. Update its uniqueness score; we choose the lowest uniqueness score across all tested contacts.
+                        if (distinctiveness < remainingContact.Uniqueness)
+                            remainingContact.Uniqueness = distinctiveness;
+                        if (remainingContact.Uniqueness > bestScore)
+                        {
+                            bestScore = remainingContact.Uniqueness;
+                            bestScoreIndex = i;
+                        }
+                    }
+                }
+                UseContact(ref remainingContacts, bestScoreIndex, ref Children, manifold);
+            }
+
+            remainingContacts.Dispose(pool);
+        }
+
+
         public unsafe void Flush<TCallbacks>(int pairId, ref CollisionBatcher<TCallbacks> batcher) where TCallbacks : struct, ICollisionCallbacks
         {
             Debug.Assert(ChildCount > 0);
@@ -384,7 +546,8 @@ namespace BepuPhysics.CollisionDetection
                     {
                         //ChooseBadly(&reducedManifold);
                         //ChooseDeepest(&reducedManifold);
-                        ChooseMostConstraining(&reducedManifold, batcher.Pool);
+                        //ChooseMostConstraining(&reducedManifold, batcher.Pool);
+                        ChooseMostDistinct(&reducedManifold, batcher.Pool);
                     }
 
                     //The manifold offsetB is the offset from shapeA origin to shapeB origin.
