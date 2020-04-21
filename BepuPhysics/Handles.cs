85f7b057c0eef721452fee9db60ff17c1330f4c6
Ross Nordby <norbo@bepuentertainment.com>
2020-04-21 15:02:03 -0500

    Added some handle convenience functions.

--- BepuPhysics/Handles.cs
+++ BepuPhysics/Handles.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BepuUtilities.Collections;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace BepuPhysics
 {
@@ -11,7 +11,7 @@ namespace BepuPhysics
     /// <summary>
     /// Unique identifier of a body belonging to a simulation's Bodies collection.
     /// </summary>
-    public struct BodyHandle
+    public struct BodyHandle : IEquatable<BodyHandle>, IEqualityComparerRef<BodyHandle>
     {
         /// <summary>
         /// Index in the handle-to-memory mapping table used to look up the current memory location of the body. 
@@ -24,16 +24,53 @@ namespace BepuPhysics
             Value = value;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(BodyHandle other)
+        {
+            return Value == other.Value;
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals((BodyHandle)obj);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(BodyHandle a, BodyHandle b)
+        {
+            return a.Value == b.Value;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(BodyHandle a, BodyHandle b)
+        {
+            return a.Value != b.Value;
+        }
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
         public override string ToString()
         {
             return Value.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Hash(ref BodyHandle item)
+        {
+            return item.Value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(ref BodyHandle a, ref BodyHandle b)
+        {
+            return a.Value == b.Value;
         }
     }
 
     /// <summary>
     /// Unique identifier of a static belonging to a simulation's Statics collection.
     /// </summary>
-    public struct StaticHandle
+    public struct StaticHandle : IEquatable<StaticHandle>, IEqualityComparerRef<StaticHandle>
     {
         /// <summary>
         /// Index in the handle-to-memory mapping table used to look up the current memory location of the body. 
@@ -46,16 +83,53 @@ namespace BepuPhysics
             Value = value;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(StaticHandle other)
+        {
+            return Value == other.Value;
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals((StaticHandle)obj);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(StaticHandle a, StaticHandle b)
+        {
+            return a.Value == b.Value;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(StaticHandle a, StaticHandle b)
+        {
+            return a.Value != b.Value;
+        }
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
         public override string ToString()
         {
             return Value.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Hash(ref StaticHandle item)
+        {
+            return item.Value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(ref StaticHandle a, ref StaticHandle b)
+        {
+            return a.Value == b.Value;
         }
     }
 
     /// <summary>
     /// Unique identifier of a constraint belonging to a simulation's Solver.
     /// </summary>
-    public struct ConstraintHandle
+    public struct ConstraintHandle : IEquatable<ConstraintHandle>, IEqualityComparerRef<ConstraintHandle>
     {
         /// <summary>
         /// Index in the handle-to-memory mapping table used to look up the current memory location of the constraint. 
@@ -68,9 +142,47 @@ namespace BepuPhysics
             Value = index;
         }
 
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(ConstraintHandle other)
+        {
+            return Value == other.Value;
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals((ConstraintHandle)obj);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(ConstraintHandle a, ConstraintHandle b)
+        {
+            return a.Value == b.Value;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(ConstraintHandle a, ConstraintHandle b)
+        {
+            return a.Value != b.Value;
+        }
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
         public override string ToString()
         {
             return Value.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Hash(ref ConstraintHandle item)
+        {
+            return item.Value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(ref ConstraintHandle a, ref ConstraintHandle b)
+        {
+            return a.Value == b.Value;
         }
     }
 }
