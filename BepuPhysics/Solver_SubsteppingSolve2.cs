4d8c181281d69d622639963e5c56769084edf4b2
Ross Nordby <norbo@bepuentertainment.com>
2021-07-13 10:39:41 -0500

    Dynamically uniform integration dispatch.

--- BepuPhysics/Solver_SubsteppingSolve2.cs
+++ BepuPhysics/Solver_SubsteppingSolve2.cs
@@ -9,6 +9,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
+using System.Runtime.Intrinsics.X86;
 
 namespace BepuPhysics
 {
