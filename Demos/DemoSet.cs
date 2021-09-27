0c76b60146f19fb45480997669c981d9045b6923
Ross Nordby <norbo@bepuentertainment.com>
2021-09-27 16:31:10 -0500

    Constraint test demo updating incrementally as twoification proceeds.

--- Demos/DemoSet.cs
+++ Demos/DemoSet.cs
@@ -45,6 +45,7 @@ namespace Demos
 
         public DemoSet()
         {
+            AddOption<ConstraintTestDemo>();
             AddOption<RopeTwistDemo>();
             AddOption<NewtDemo>();
             AddOption<CarDemo>();
