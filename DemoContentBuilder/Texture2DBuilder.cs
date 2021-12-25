a40dba61b9fe96c2b871cbba7c97b0dba0ca0de2
Ross Nordby <norbo@bepuentertainment.com>
2021-12-24 19:51:18 -0600

    Fixed texture2dbuilder.

--- DemoContentBuilder/Texture2DBuilder.cs
+++ DemoContentBuilder/Texture2DBuilder.cs
@@ -15,31 +15,22 @@ namespace DemoContentBuilder
     {
         public unsafe static Texture2DContent Build(Stream dataStream)
         {
-            using (var rawImage = SixLabors.ImageSharp.Image.Load(dataStream))
-            using (var image = rawImage.CloneAs<Rgba32>())
+            using var rawImage = SixLabors.ImageSharp.Image.Load(dataStream);
+            using var image = rawImage.CloneAs<Rgba32>();
+            //We're only supporting R8G8B8A8 right now, so texel size in bytes is always 4.
+            //We don't compute mips during at content time. We could, but... there's not much reason to.
+            //The font builder does because it uses a nonstandard mip process, but this builder is expected to be used to with normal data.
+            var content = new Texture2DContent(image.Width, image.Height, 1, sizeof(Rgba32));
+            var data = (Rgba32*)content.Pin();
+            //Copy the image data into the Texture2DContent.
+            for (int rowIndex = 0; rowIndex < image.Height; ++rowIndex)
             {
-                var pixels = image.GetPixelSpan();
-                var imageData = new int[pixels.Length];
-                fixed (int* imageDataPointer = imageData)
-                {
-                    var casted = new Span<Rgba32>(imageDataPointer, imageData.Length);
-                    pixels.CopyTo(casted);
-                }
-                //We're only supporting R8G8B8A8 right now, so texel size in bytes is always 4.
-                //We don't compute mips during at content time. We could, but... there's not much reason to.
-                //The font builder does because it uses a nonstandard mip process, but this builder is expected to be used to with normal data.
-                var content = new Texture2DContent(image.Width, image.Height, 1, sizeof(Rgba32));
-                var data = (Rgba32*)content.Pin();
-                //Copy the image data into the Texture2DContent.
-                for (int rowIndex = 0; rowIndex < image.Height; ++rowIndex)
-                {
-                    var sourceRow = image.GetPixelRowSpan(rowIndex);
-                    var targetRow = data + content.GetRowOffsetForMip0(rowIndex);
-                    Unsafe.CopyBlockUnaligned(ref *(byte*)targetRow, ref Unsafe.As<Rgba32, byte>(ref sourceRow[0]), (uint)(sizeof(Rgba32) * image.Width));
-                }
-                content.Unpin();
-                return content;
+                var sourceRow = image.GetPixelRowSpan(rowIndex);
+                var targetRow = data + content.GetRowOffsetForMip0(rowIndex);
+                Unsafe.CopyBlockUnaligned(ref *(byte*)targetRow, ref Unsafe.As<Rgba32, byte>(ref sourceRow[0]), (uint)(sizeof(Rgba32) * image.Width));
             }
+            content.Unpin();
+            return content;
         }
     }
 }
