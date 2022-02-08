b86a90b016f0c6783d9d79e15eb67093d959e208
Ross Nordby <norbo@bepuentertainment.com>
2022-02-08 16:26:24 -0600

    Media updates.

---
+++ Demos/SpecializedTests/Media/2.0/NewtDemandingSacrificeVideoDemo.cs
@@ -0,0 +1,78 @@
+﻿using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuUtilities;
+using DemoContentLoader;
+using DemoRenderer;
+using Demos.Demos;
+using DemoUtilities;
+using System;
+using System.Numerics;
+
+namespace Demos.SpecializedTests.Media
+{
+    public class NewtDemandingSacrificeVideoDemo : Demo
+    {
+        CollidableProperty<SubgroupCollisionFilter> filters;
+        public unsafe override void Initialize(ContentArchive content, Camera camera)
+        {
+            camera.Position = new Vector3(-32f, 20.5f, 61f);
+            camera.Yaw = MathHelper.Pi * 0.3f;
+            camera.Pitch = MathHelper.Pi * -0.05f;
+
+            filters = new CollidableProperty<SubgroupCollisionFilter>(BufferPool);
+            Simulation = Simulation.Create(BufferPool, new SubgroupFilteredCallbacks(filters), new DemoPoseIntegratorCallbacks(new Vector3(0, -10, 0)), new SolveDescription(4, 1));
+
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, -0.5f, 0), Simulation.Shapes.Add(new Box(1500, 1, 1500))));
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, 10, 0), Simulation.Shapes.Add(new Box(70, 20, 80))));
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, 7.5f, 0), Simulation.Shapes.Add(new Box(80, 15, 90))));
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, 5, 0), Simulation.Shapes.Add(new Box(90, 10, 100))));
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, 2.5f, 0), Simulation.Shapes.Add(new Box(100, 5, 110))));
+
+            //High fidelity simulation isn't super important on this one.
+            Simulation.Solver.VelocityIterationCount = 2;
+
+            DemoMeshHelper.LoadModel(content, BufferPool, "Content\\newt.obj", new Vector3(30), out var mesh);
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, 20, 0), QuaternionEx.CreateFromAxisAngle(Vector3.UnitY, 0), Simulation.Shapes.Add(mesh)));
+        }
+
+        Random random = new Random(5);
+        int ragdollIndex = 0;
+
+        BodyVelocity GetRandomizedVelocity(in Vector3 linearVelocity)
+        {
+            return new BodyVelocity { Linear = linearVelocity, Angular = new Vector3(-20) + 40 * new Vector3(random.NextSingle(), random.NextSingle(), random.NextSingle()) };
+        }
+
+        public override void Update(Window window, Camera camera, Input input, float dt)
+        {
+            var pose = TestHelpers.CreateRandomPose(random, new BoundingBox
+            {
+                Min = new Vector3(-10, 5, 70),
+                Max = new Vector3(10, 15, 70)
+            });
+            var linearVelocity = Vector3.Normalize(new Vector3(-2 + 4 * random.NextSingle(), 31 + 4 * random.NextSingle(), 50) - pose.Position) * 40;
+            var handles = RagdollDemo.AddRagdoll(pose.Position, pose.Orientation, ragdollIndex++, filters, Simulation);
+            var bodies = Simulation.Bodies;
+            //This could be done better, but...  ... .... ..........
+            bodies[handles.Hips].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.Abdomen].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.Chest].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.Head].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.LeftArm.UpperArm].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.LeftArm.LowerArm].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.LeftArm.Hand].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.RightArm.UpperArm].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.RightArm.LowerArm].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.RightArm.Hand].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.LeftLeg.UpperLeg].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.LeftLeg.LowerLeg].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.LeftLeg.Foot].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.RightLeg.UpperLeg].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.RightLeg.LowerLeg].Velocity = GetRandomizedVelocity(linearVelocity);
+            bodies[handles.RightLeg.Foot].Velocity = GetRandomizedVelocity(linearVelocity);
+
+            base.Update(window, camera, input, dt);
+        }
+
+    }
+}
---
+++ Demos/SpecializedTests/Media/2.0/PyramidVideoDemo.cs
@@ -0,0 +1,84 @@
+﻿using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuPhysics.Constraints;
+using BepuUtilities;
+using DemoContentLoader;
+using DemoRenderer;
+using DemoRenderer.UI;
+using DemoUtilities;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Demos.Demos.Media
+{
+    /// <summary>
+    /// A pyramid of boxes, because you can't have a physics engine without pyramids of boxes.
+    /// </summary>
+    public class PyramidVideoDemo : Demo
+    {
+        public unsafe override void Initialize(ContentArchive content, Camera camera)
+        {
+            camera.Position = new Vector3(-70, 8, 318);
+            camera.Yaw = MathHelper.Pi * 1f / 4;
+            camera.Pitch = 0;
+            Simulation = Simulation.Create(BufferPool, new DemoNarrowPhaseCallbacks(new SpringSettings(30, 1)), new DemoPoseIntegratorCallbacks(new Vector3(0, -10, 0)), new SolveDescription(8, 1));
+
+            var boxShape = new Box(1, 1, 1);
+            var boxInertia = boxShape.ComputeInertia(1);
+            var boxIndex = Simulation.Shapes.Add(boxShape);
+            const int pyramidCount = 120;
+            for (int pyramidIndex = 0; pyramidIndex < pyramidCount; ++pyramidIndex)
+            {
+                const int rowCount = 20;
+                for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+                {
+                    int columnCount = rowCount - rowIndex;
+                    for (int columnIndex = 0; columnIndex < columnCount; ++columnIndex)
+                    {
+                        Simulation.Bodies.Add(BodyDescription.CreateDynamic(new Vector3(
+                            (-columnCount * 0.5f + columnIndex) * boxShape.Width,
+                            (rowIndex + 0.5f) * boxShape.Height,
+                            (pyramidIndex - pyramidCount * 0.5f) * (boxShape.Length + 4)),
+                            boxInertia, boxIndex, 0.01f));
+                    }
+                }
+            }
+
+            Simulation.Statics.Add(new StaticDescription(new Vector3(0, -0.5f, 0), Simulation.Shapes.Add(new Box(2500, 1, 2500))));
+        }
+
+        //We'll randomize the size of bullets.
+        Random random = new Random(5);
+        public override void Update(Window window, Camera camera, Input input, float dt)
+        {
+            if (input != null && input.WasPushed(OpenTK.Input.Key.Z))
+            {
+                //Create the shape that we'll launch at the pyramids when the user presses a button.
+                var bulletShape = new Sphere(6);
+                //Note that the use of radius^3 for mass can produce some pretty serious mass ratios. 
+                //Observe what happens when a large ball sits on top of a few boxes with a fraction of the mass-
+                //the collision appears much squishier and less stable. For most games, if you want to maintain rigidity, you'll want to use some combination of:
+                //1) Limit the ratio of heavy object masses to light object masses when those heavy objects depend on the light objects.
+                //2) Use a shorter timestep duration and update more frequently.
+                //3) Use a greater number of solver iterations.
+                //#2 and #3 can become very expensive. In pathological cases, it can end up slower than using a quality-focused solver for the same simulation.
+                //Unfortunately, at the moment, bepuphysics v2 does not contain any alternative solvers, so if you can't afford to brute force the the problem away,
+                //the best solution is to cheat as much as possible to avoid the corner cases.
+                var bodyDescription = BodyDescription.CreateConvexDynamic(
+                    new Vector3(0, 8, -500), new Vector3(0, 0, 110), 50000, Simulation.Shapes, bulletShape);
+                Simulation.Bodies.Add(bodyDescription);
+            }
+            base.Update(window, camera, input, dt);
+        }
+
+        public override void Render(Renderer renderer, Camera camera, Input input, TextBuilder text, Font font)
+        {
+            text.Clear().Append("Press Z to launch a ball!");
+            renderer.TextBatcher.Write(text, new Vector2(20, renderer.Surface.Resolution.Y - 20), 16, new Vector3(1, 1, 1), font);
+            base.Render(renderer, camera, input, text, font);
+        }
+
+    }
+}
