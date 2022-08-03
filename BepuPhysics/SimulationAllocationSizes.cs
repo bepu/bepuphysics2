79185b126dcc6ccd8353a611c476ffb7bb7f5ed4
Ross Nordby <norbo@bepuentertainment.com>
2022-08-03 12:16:04 -0500

    Comment oopsy.

--- BepuPhysics/SimulationAllocationSizes.cs
+++ BepuPhysics/SimulationAllocationSizes.cs
@@ -45,7 +45,7 @@ namespace BepuPhysics
         /// Constructs a description of simulation allocations.
         /// </summary>
         /// <param name="bodies">The number of bodies to allocate space for.</param>
-        /// <param name="statics">The number of inactive islands to allocate space for.</param>
+        /// <param name="statics">The number of statics to allocate space for.</param>
         /// <param name="islands">The number of inactive islands to allocate space for.</param>
         /// <param name="shapesPerType">Minimum number of shapes to allocate space for in each shape type batch.</param>
         /// <param name="constraints">The number of constraints to allocate bookkeeping space for. This does not affect actual type batch allocation sizes, only the solver-level constraint handle storage.</param>
