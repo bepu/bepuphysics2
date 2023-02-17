e9852ee1bcdc73bf89c83b1d377e5e49e7400a23
Ross Nordby <norbo@bepuentertainment.com>
2023-02-17 15:36:36 -0600

    SolverContactEnumerationDemo now contains its relevant types to protect the namespace a bit.

--- Demos/Demos/SolverContactEnumerationDemo.cs
+++ Demos/Demos/SolverContactEnumerationDemo.cs
@@ -13,7 +13,13 @@ using DemoUtilities;
 using System;
 using System.Numerics;
 
-namespace Demos.Demos
+namespace Demos.Demos;
+
+
+/// <summary>
+/// Shows how to enumerate contact constraints in the solver using contact accessors and <see cref="ISolverContactDataExtractor"/>.
+/// </summary>
+public class SolverContactEnumerationDemo : Demo
 {
     /// <summary>
     /// Stores the easy to read data associated with a single contact extracted from solver contact constraints.
@@ -186,104 +192,98 @@ namespace Demos.Demos
         }
     }
 
-    /// <summary>
-    /// Shows how to enumerate contact constraints in the solver using contact accessors and ISolverContactDataExtractor.
-    /// </summary>
-    public class SolverContactEnumerationDemo : Demo
+    BodyHandle sensorBodyHandle;
+    public override void Initialize(ContentArchive content, Camera camera)
     {
-        BodyHandle sensorBodyHandle;
-        public override void Initialize(ContentArchive content, Camera camera)
+        camera.Position = new Vector3(0, 5, 25);
+        camera.Yaw = 0;
+        camera.Pitch = 0;
+
+        Simulation = Simulation.Create(BufferPool, new DemoNarrowPhaseCallbacks(new SpringSettings(30, 1)), new DemoPoseIntegratorCallbacks(new Vector3(0, -10, 0)), new SolveDescription(8, 1));
+
+        //Drop a pyramid on top of the sensor so there are more contacts to look at.
+        var boxShape = new Box(1, 1, 1);
+        var boxInertia = boxShape.ComputeInertia(1);
+        var boxIndex = Simulation.Shapes.Add(boxShape);
+
+        const int rowCount = 20;
+        for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
         {
-            camera.Position = new Vector3(0, 5, 25);
-            camera.Yaw = 0;
-            camera.Pitch = 0;
-
-            Simulation = Simulation.Create(BufferPool, new DemoNarrowPhaseCallbacks(new SpringSettings(30, 1)), new DemoPoseIntegratorCallbacks(new Vector3(0, -10, 0)), new SolveDescription(8, 1));
-
-            //Drop a pyramid on top of the sensor so there are more contacts to look at.
-            var boxShape = new Box(1, 1, 1);
-            var boxInertia = boxShape.ComputeInertia(1);
-            var boxIndex = Simulation.Shapes.Add(boxShape);
-
-            const int rowCount = 20;
-            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+            int columnCount = rowCount - rowIndex;
+            for (int columnIndex = 0; columnIndex < columnCount; ++columnIndex)
             {
-                int columnCount = rowCount - rowIndex;
-                for (int columnIndex = 0; columnIndex < columnCount; ++columnIndex)
-                {
-                    Simulation.Bodies.Add(BodyDescription.CreateDynamic(new Vector3(
-                        (-columnCount * 0.5f + columnIndex) * boxShape.Width,
-                        (rowIndex + 0.5f) * boxShape.Height + 10, 0),
-                        boxInertia, boxIndex, 0.01f));
-                }
+                Simulation.Bodies.Add(BodyDescription.CreateDynamic(new Vector3(
+                    (-columnCount * 0.5f + columnIndex) * boxShape.Width,
+                    (rowIndex + 0.5f) * boxShape.Height + 10, 0),
+                    boxInertia, boxIndex, 0.01f));
             }
-
-
-            sensorBodyHandle = Simulation.Bodies.Add(BodyDescription.CreateConvexDynamic(new Vector3(0, 0, 1), 10, Simulation.Shapes, new Box(4, 2, 6)));
-
-            //Put a mesh under the sensor so that nonconvex contacts are shown.
-            const int planeWidth = 128;
-            const int planeHeight = 128;
-            DemoMeshHelper.CreateDeformedPlane(planeWidth, planeHeight,
-                (int x, int y) =>
-                {
-                    return new Vector3(x - planeWidth / 2, 1 * MathF.Cos(x / 2f) * MathF.Sin(y / 2f), y - planeHeight / 2);
-                }, new Vector3(2, 1, 2), BufferPool, out var planeMesh);
-            Simulation.Statics.Add(new StaticDescription(new Vector3(0, -2, 0), QuaternionEx.CreateFromAxisAngle(new Vector3(0, 1, 0), MathF.PI / 2), Simulation.Shapes.Add(planeMesh)));
         }
 
 
-        public override void Render(Renderer renderer, Camera camera, Input input, TextBuilder text, Font font)
+        sensorBodyHandle = Simulation.Bodies.Add(BodyDescription.CreateConvexDynamic(new Vector3(0, 0, 1), 10, Simulation.Shapes, new Box(4, 2, 6)));
+
+        //Put a mesh under the sensor so that nonconvex contacts are shown.
+        const int planeWidth = 128;
+        const int planeHeight = 128;
+        DemoMeshHelper.CreateDeformedPlane(planeWidth, planeHeight,
+            (int x, int y) =>
+            {
+                return new Vector3(x - planeWidth / 2, 1 * MathF.Cos(x / 2f) * MathF.Sin(y / 2f), y - planeHeight / 2);
+            }, new Vector3(2, 1, 2), BufferPool, out var planeMesh);
+        Simulation.Statics.Add(new StaticDescription(new Vector3(0, -2, 0), QuaternionEx.CreateFromAxisAngle(new Vector3(0, 1, 0), MathF.PI / 2), Simulation.Shapes.Add(planeMesh)));
+    }
+
+
+    public override void Render(Renderer renderer, Camera camera, Input input, TextBuilder text, Font font)
+    {
+        var sensorBody = Simulation.Bodies[sensorBodyHandle];
+        var extractor = new SolverContactDataExtractor(BufferPool, sensorBody.Constraints.Count);
+        //The basic idea behind the contact extractor is to submit it to a narrow phase contact accessor that is able to understand the solver's layout,
+        //which will then call the contact extractor's relevant callbacks for the type of constraint encountered.
+        //Here, we'll enumerate over all the constraints currently affecting the sensor body, attempting to extract contact data from each one.
+        //If there are constraints that aren't contact constraints, they'll just get skipped.
+        for (int i = 0; i < sensorBody.Constraints.Count; ++i)
         {
-            var sensorBody = Simulation.Bodies[sensorBodyHandle];
-            var extractor = new SolverContactDataExtractor(BufferPool, sensorBody.Constraints.Count);
-            //The basic idea behind the contact extractor is to submit it to a narrow phase contact accessor that is able to understand the solver's layout,
-            //which will then call the contact extractor's relevant callbacks for the type of constraint encountered.
-            //Here, we'll enumerate over all the constraints currently affecting the sensor body, attempting to extract contact data from each one.
-            //If there are constraints that aren't contact constraints, they'll just get skipped.
-            for (int i = 0; i < sensorBody.Constraints.Count; ++i)
-            {
-                Simulation.NarrowPhase.TryExtractSolverContactData(sensorBody.Constraints[i].ConnectingConstraintHandle, ref extractor);
-            }
-            //We now have extracted contact data. Let's analyze it!
-            //For the purposes of the demo, we'll draw debug shapes at the contacts to represent the different properties.
-            int sensorContactCount = 0;
-            for (int manifoldIndex = 0; manifoldIndex < extractor.Constraints.Count; ++manifoldIndex)
-            {
-                ref var constraintContacts = ref extractor.Constraints[manifoldIndex];
-                var bodyA = Simulation.Bodies[constraintContacts.BodyA];
-                sensorContactCount += constraintContacts.Contacts.Count;
-                for (int contactIndex = 0; contactIndex < constraintContacts.Contacts.Count; ++contactIndex)
-                {
-                    ref var contact = ref constraintContacts.Contacts[contactIndex];
-                    var contactPosition = contact.OffsetA + bodyA.Pose.Position;
-                    Matrix3x3 basisPose;
-                    //We want to visualize both friction and penetration impulses, so a cylinder is a good choice- radius and length.
-                    //The length will be oriented along the contact normal, while the radius will expand along the tangent directions.
-                    BepuPhysics.Helpers.BuildOrthonormalBasis(contact.Normal, out basisPose.X, out basisPose.Z);
-                    basisPose.Y = contact.Normal;
-                    var baseLength = 0.2f;
-                    var baseRadius = 0.1f;
-                    //We'll make purely speculative contacts (negative depth) a fixed minimum size and a different color.
-                    var contactVisualShape = contact.Depth < 0 ?
-                        new Cylinder(baseRadius, baseLength) :
-                        new Cylinder(baseRadius + MathF.Min(5, contact.FrictionImpulseMagnitude * 0.1f), baseLength + MathF.Min(5, contact.PenetrationImpulse * 0.3f));
-                    RigidPose contactVisualPose;
-                    contactVisualPose.Position = contactPosition + contact.Normal * contactVisualShape.HalfLength;
-                    QuaternionEx.CreateFromRotationMatrix(basisPose, out contactVisualPose.Orientation);
-                    renderer.Shapes.AddShape(contactVisualShape, Simulation.Shapes, contactVisualPose, contact.Depth < 0 ? new Vector3(0, 0, 1) : new Vector3(0, 1, 0));
-                }
-            }
-
-            extractor.Dispose();
-
-            var resolution = renderer.Surface.Resolution;
-            renderer.TextBatcher.Write(text.Clear().Append("The solver stores data in an optimized array-of-structures-of-arrays format that makes it difficult to directly read."), new Vector2(16, resolution.Y - 96), 16, Vector3.One, font);
-            renderer.TextBatcher.Write(text.Clear().Append("This demo implements an ISolverContactDataExtractor that pulls data out of the solver and puts it into a simpler format."), new Vector2(16, resolution.Y - 80), 16, Vector3.One, font);
-            renderer.TextBatcher.Write(text.Clear().Append("Pulling data this way can sometimes be more convenient than tracking contacts from INarrowPhaseCallbacks."), new Vector2(16, resolution.Y - 64), 16, Vector3.One, font);
-            renderer.TextBatcher.Write(text.Clear().Append("Contacts associated with the large box are visualized. The size of a contact corresponds to the contact's force."), new Vector2(16, resolution.Y - 48), 16, Vector3.One, font);
-            renderer.TextBatcher.Write(text.Clear().Append("Sensor manifold constraint count: ").Append(extractor.Constraints.Count).Append(", contact count: ").Append(sensorContactCount), new Vector2(16, resolution.Y - 20), 20, Vector3.One, font);
-            base.Render(renderer, camera, input, text, font);
+            Simulation.NarrowPhase.TryExtractSolverContactData(sensorBody.Constraints[i].ConnectingConstraintHandle, ref extractor);
         }
+        //We now have extracted contact data. Let's analyze it!
+        //For the purposes of the demo, we'll draw debug shapes at the contacts to represent the different properties.
+        int sensorContactCount = 0;
+        for (int manifoldIndex = 0; manifoldIndex < extractor.Constraints.Count; ++manifoldIndex)
+        {
+            ref var constraintContacts = ref extractor.Constraints[manifoldIndex];
+            var bodyA = Simulation.Bodies[constraintContacts.BodyA];
+            sensorContactCount += constraintContacts.Contacts.Count;
+            for (int contactIndex = 0; contactIndex < constraintContacts.Contacts.Count; ++contactIndex)
+            {
+                ref var contact = ref constraintContacts.Contacts[contactIndex];
+                var contactPosition = contact.OffsetA + bodyA.Pose.Position;
+                Matrix3x3 basisPose;
+                //We want to visualize both friction and penetration impulses, so a cylinder is a good choice- radius and length.
+                //The length will be oriented along the contact normal, while the radius will expand along the tangent directions.
+                BepuPhysics.Helpers.BuildOrthonormalBasis(contact.Normal, out basisPose.X, out basisPose.Z);
+                basisPose.Y = contact.Normal;
+                var baseLength = 0.2f;
+                var baseRadius = 0.1f;
+                //We'll make purely speculative contacts (negative depth) a fixed minimum size and a different color.
+                var contactVisualShape = contact.Depth < 0 ?
+                    new Cylinder(baseRadius, baseLength) :
+                    new Cylinder(baseRadius + MathF.Min(5, contact.FrictionImpulseMagnitude * 0.1f), baseLength + MathF.Min(5, contact.PenetrationImpulse * 0.3f));
+                RigidPose contactVisualPose;
+                contactVisualPose.Position = contactPosition + contact.Normal * contactVisualShape.HalfLength;
+                QuaternionEx.CreateFromRotationMatrix(basisPose, out contactVisualPose.Orientation);
+                renderer.Shapes.AddShape(contactVisualShape, Simulation.Shapes, contactVisualPose, contact.Depth < 0 ? new Vector3(0, 0, 1) : new Vector3(0, 1, 0));
+            }
+        }
+
+        extractor.Dispose();
+
+        var resolution = renderer.Surface.Resolution;
+        renderer.TextBatcher.Write(text.Clear().Append("The solver stores data in an optimized array-of-structures-of-arrays format that makes it difficult to directly read."), new Vector2(16, resolution.Y - 96), 16, Vector3.One, font);
+        renderer.TextBatcher.Write(text.Clear().Append("This demo implements an ISolverContactDataExtractor that pulls data out of the solver and puts it into a simpler format."), new Vector2(16, resolution.Y - 80), 16, Vector3.One, font);
+        renderer.TextBatcher.Write(text.Clear().Append("Pulling data this way can sometimes be more convenient than tracking contacts from INarrowPhaseCallbacks."), new Vector2(16, resolution.Y - 64), 16, Vector3.One, font);
+        renderer.TextBatcher.Write(text.Clear().Append("Contacts associated with the large box are visualized. The size of a contact corresponds to the contact's force."), new Vector2(16, resolution.Y - 48), 16, Vector3.One, font);
+        renderer.TextBatcher.Write(text.Clear().Append("Sensor manifold constraint count: ").Append(extractor.Constraints.Count).Append(", contact count: ").Append(sensorContactCount), new Vector2(16, resolution.Y - 20), 20, Vector3.One, font);
+        base.Render(renderer, camera, input, text, font);
     }
 }
