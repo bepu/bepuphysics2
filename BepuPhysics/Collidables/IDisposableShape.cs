cf5d7b637a301e900db463c43bfb457d82f846b5
Salvage <29021710+Saalvage@users.noreply.github.com>
2023-10-09 17:13:40 +0200

    Introduce `IDisposableShape`
    Allows for properly disposing of a shape, given only an `IShape` without having to do various type checks.

---
+++ BepuPhysics/Collidables/IDisposableShape.cs
@@ -0,0 +1,13 @@
+﻿using BepuUtilities.Memory;
+
+namespace BepuPhysics.Collidables
+{
+	public interface IDisposableShape : IShape
+	{
+		/// <summary>
+		/// Returns all resources used by the shape instance to the given pool.
+		/// </summary>
+		/// <param name="pool">Pool to return shape resources to.</param>
+		void Dispose(BufferPool pool);
+	}
+}
