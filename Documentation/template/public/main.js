f64b6161fb00268e9ed35cc42df1a04c9b15ac94
Vaclav Elias <vaclavelias@gmail.com>
2024-12-29 21:48:50 +0000

    feat: Docfx implementation added

---
+++ Documentation/template/public/main.js
@@ -0,0 +1,17 @@
+const app = {
+    languageDropdownCreated: false,
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/bepu/bepuphysics2',
+            title: 'GitHub'
+        },
+        {
+            icon: 'discord',
+            href: 'https://discord.gg/ssa2XpY',
+            title: 'Discord'
+        }
+    ]
+};
+
+export default app;
