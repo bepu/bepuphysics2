8e5a217687ec49ffb09326c12ee848b1c5628c1d
Nigel-Bess <nigel.bess@fulfil.com>
2025-11-05 23:00:08 -0800

    started on demo setup

---
+++ Demos.GL/Broken/CompoundFallsThroughFloor.cs
@@ -0,0 +1,55 @@
+﻿
+using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuUtilities;
+using DemoContentLoader;
+using DemoRenderer;
+using DemoUtilities;
+using System.Numerics;
+
+namespace Demos.Broken;
+
+internal class CompoundFallsThroughFloor : Demo
+{
+    private BodyActivityDescription NeverSleep() => new BodyActivityDescription(sleepThreshold: float.MinValue, minimumTimestepCountUnderThreshold: byte.MaxValue);
+
+    public override void Initialize(ContentArchive content, Camera camera)
+    {
+        camera.Position = new Vector3(-1000f, 400, -1000f);
+        camera.Yaw = MathHelper.Pi * 3f / 4;
+        camera.Pitch = MathHelper.Pi * 0.1f;
+
+        var narrowPhaseCallbacks = new SimpleNarrowPhaseCallbacks(
+            frictionCoefficient: 0.3f,
+            maximumRecoveryVelocity: 50,
+            impactFrequency: 20,
+            impactDampingRatio: 0.5f
+            );
+
+        var gravity = new Vector3(0, -9807f, 0); // mm/s
+        var timeStepSeconds = .12f;
+
+        Simulation = Simulation.Create(BufferPool, narrowPhaseCallbacks, new PoseIntegratorCallbacks(gravity: gravity, timeStepSeconds: timeStepSeconds), new SolveDescription(8, 1));
+        var floor = new Box(1000, 9, 1000);
+
+        Simulation.Statics.Add(new StaticDescription(RigidPose.Identity, Simulation.Shapes.Add(floor)));
+
+        // Item as a standalone (not compound)
+        {
+            var rawCollidableBox = new Box(100, 130, 100);
+            var boxPose = new RigidPose(new Vector3(-500, rawCollidableBox.HalfHeight + 18, -500), Quaternion.Identity);
+            var boxInertia = rawCollidableBox.ComputeInertia(1);
+            var boxIndex = Simulation.Shapes.Add(rawCollidableBox);
+            var collisionDetection = new ContinuousDetection();
+            var boxCollidable = new CollidableDescription(boxIndex, collisionDetection);
+            var boxDescription = BodyDescription.CreateDynamic(boxPose, boxInertia, boxCollidable, NeverSleep());
+            Simulation.Bodies.Add(boxDescription);
+        }
+
+    }
+
+    public override void Update(Window window, Camera camera, Input input, float dt)
+    {
+        base.Update(window, camera, input, dt);
+    }
+}
--- Demos/Program.cs
+++ Demos/Program.cs
@@ -2,12 +2,41 @@
 using DemoContentLoader;
 using DemoUtilities;
 using OpenTK;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Demos;
 
 class Program
 {
-    static void Main()
+
+
+    static async Task Main()
+    {
+        var window = new Window("pretty cool multicolored window",
+            new Int2((int)(DisplayDevice.Default.Width * 0.75f), (int)(DisplayDevice.Default.Height * 0.75f)), WindowMode.Windowed);
+        var loop = new GameLoop(window);
+        ContentArchive content;
+        using (var stream = typeof(Program).Assembly.GetManifestResourceStream("Demos.Demos.contentarchive"))
+        {
+            content = ContentArchive.Load(stream);
+        }
+        //HeadlessTest.Test<ShapePileTestDemo>(content, 4, 32, 512);
+        var demo = new DemoHarness(loop, content);
+        var dt = 0.12f;
+        loop.Update(dt);
+
+        while (true)
+        {
+            loop.SingleFrame(demo, dt);
+            Thread.Sleep(1000);
+        }
+
+        loop.Dispose();
+        window.Dispose();
+    }
+
+    static void OldMain()
     {
         var window = new Window("pretty cool multicolored window",
             new Int2((int)(DisplayDevice.Default.Width * 0.75f), (int)(DisplayDevice.Default.Height * 0.75f)), WindowMode.Windowed);
